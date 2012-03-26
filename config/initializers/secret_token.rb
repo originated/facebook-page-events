90d1647a67a0f26d14f8ccbc5971312fef2295fb
Cifani <mcifani@gmail.com>
2012-03-26 15:25:42 -0700

    fbgraph app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbapp::Application.config.secret_token = '9fd3a0661b4129ee8e2797cbbbd9dfbffb2bed3c1e7444ba4f5536e736875e758fb9ef7247a4605b68edb4d318b02d9190255d4909a31fc99ac5910a53121487'
